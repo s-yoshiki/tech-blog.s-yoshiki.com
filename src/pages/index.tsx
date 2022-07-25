78263828b433750869317659f747cacf9ff4a1a3
Yoshiki Shinagawa <s.yoshiki1123@gmail.com>
2022-07-26 01:35:48 +0900

    fix: pagenation 再追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,15 @@ import Badge from 'components/badge';
 import Author from 'components/author';
 import Link from "next/link"
 import YearMonthPosts from 'components/yesar-month-posts';
+import React, { useState } from 'react';
+import { Posts } from 'types/entry.interface';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
+const postsPerPage = 15
+
 export const getStaticProps = async () => {
-  const allPosts = PostsManager.getData().slice(0, 15)
+  const allPosts = PostsManager.getData()
   const tags = PostsManager.getCountsGroupByTags().slice(0, 50)
   const dates = PostsManager.getCountsGroupYearMonth()
   const popular = PostsManager.getPopularPosts().slice(0, 10)
@@ -32,7 +36,33 @@ const MiddleHeadding = ({ children }: { children: string }) => {
   )
 }
 
+const NewPosts = ({posts, count}: {posts: Posts[], count: number}) => {
+  const text = []
+  for (let i = 0; i < count; i++) {
+    text.push(
+      <PostsBand posts={posts.slice(postsPerPage * i, postsPerPage * (i + 1) )} /> 
+    )
+  }
+  return (
+    <>
+      {text.map(e => {
+        return <>
+          {e}
+          <div className="p-8"></div>
+        </>
+      })}
+    </>
+  )
+}
+
 const Home: NextPage<Props> = ({ allPosts, tags, dates, popular }) => {
+  const maxPage = Math.ceil(allPosts.length / postsPerPage)
+  const [count, _setCount] = useState(1);
+  const setCount = (num: number) => {
+    if (num <= maxPage) {
+      _setCount(num)
+    }
+  }
   return (
     <Layout>
       <div>
@@ -42,7 +72,16 @@ const Home: NextPage<Props> = ({ allPosts, tags, dates, popular }) => {
             <Search onClick={searchEventHandler} />
           </div>
           <MiddleHeadding>New Posts</MiddleHeadding>
-          <PostsBand posts={allPosts} />
+          <NewPosts  posts={allPosts} count={count}/>
+          <div className="flex justify-center p-8">
+            <div>
+            <button 
+              onClick={() => setCount(count + 1)}
+              className="bg-blue-500 w-96 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+              Show more... {count} / {maxPage}
+            </button>
+            </div>
+          </div>
         </div>
         <div className='p-8'></div>
       </div>
