1bf8887fcd4530a4d2f7b911a7bf05cefb33c1d8
Yoshiki Shinagawa <s.yoshiki1123@gmail.com>
2025-04-26 15:26:08 +0900

    fix: component error

--- src/components/entry/right-bar.tsx
+++ src/components/entry/right-bar.tsx
@@ -37,7 +37,7 @@ const RightBar = () => {
             >
               Contact
             </a>
-            <div className="relative" x-data="{ open: false }">
+            <div className="relative">
               <button className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                 <span>Dropdown</span>
                 <svg
